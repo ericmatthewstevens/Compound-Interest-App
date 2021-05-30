949ef677318cd90feafc55857df87b8c6dd3e225
SchnitzelB <es971120@icloud.com>
2021-05-30 13:46:54 -0500

    Initial commit

---
+++ src/main/java/com/example/Tomcat_EE_App/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.Tomcat_EE_App;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
